52cfac50848f31ab7efe5c2f288ec39a10ab44cf
Jishan_Mullani <mahamadjishan007@gmail.com>
2026-04-02 11:14:27 +0530

    Add files via upload

---
+++ Database_infomarion.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE customer_behavior;
+USE customer_behavior;
+
+-- Q1. Total revenue by gender
+SELECT gender, SUM(purchase_amount) AS revenue
+FROM customer
+GROUP BY gender;
+
+-- Q2. What is the revenue contribution of each age group? 
+SELECT 
+    age_group,
+    SUM(purchase_amount) AS total_revenue
+FROM customer
+GROUP BY age_group
+ORDER BY total_revenue desc;
+
+-- Q3. Are customers who are repeat buyers (more than 5 previous purchases) also likely to subscribe?
+SELECT subscription_status,
+       COUNT(customer_id) AS repeat_buyers
+FROM customer
+WHERE previous_purchases > 5
+GROUP BY subscription_status;
